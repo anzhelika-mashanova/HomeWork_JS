7daa66ac022bec164a61a7f076a6a6dfa8f68aab
anzhelika-mashanova <len4ik272523@gmail.com>
2021-09-18 00:57:48 +0300

    home work 26

---
+++ lesson_26/app.js
@@ -0,0 +1,6 @@
+import * as calculator from "./calculator.js";
+
+console.log(calculator.add(10, 2));
+console.log(calculator.sub(12, 5));
+console.log(calculator.mult(7, 4));
+console.log(calculator.div(28, 2));
---
+++ lesson_26/calculator.js
@@ -0,0 +1,9 @@
+import {add} from "./add.js";
+import {sub} from "./sub.js";
+import {mult} from "./mult.js";
+import {div} from "./div.js";
+
+export {add};
+export {sub};
+export {mult};
+export {div};
